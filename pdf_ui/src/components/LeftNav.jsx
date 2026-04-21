e804495ceca24077d057c2d8c7b5d8940330c95a
Khalid <Khalid.ahmadzai@ucop.edu>
2026-04-20 17:06:32 -0700

    initial commit

--- pdf_ui/src/components/LeftNav.jsx
+++ pdf_ui/src/components/LeftNav.jsx
@@ -91,7 +91,7 @@ const LeftNav = ({ isCollapsed, setIsCollapsed, mobileOpen, setMobileOpen }) => 
             </Box>
             <Divider sx={{ marginBottom: 2 }} />
             <Typography variant="body2" gutterBottom>
-              - Each user is limited to <strong>8 PDF document uploads</strong>.
+              - Each user is limited to <strong>10 PDF document uploads</strong>.
             </Typography>
             <Typography variant="body2" gutterBottom>
               - Documents cannot exceed <strong>10 pages</strong>.
